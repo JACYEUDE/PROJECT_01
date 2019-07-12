3085733c92710b2374f3fa0747f28d79a31f6c65
JACYEUDE <jacyeudearaujo@hotmail.com>
2019-07-12 10:35:31 -0300

    ..

--- ADXL345lib_uptotxt/ADXL345lib_/ADXL345lib_.ino
+++ ADXL345lib_uptotxt/ADXL345lib_/ADXL345lib_.ino
@@ -16,33 +16,16 @@ void setup()
     //      OSEPP_ACC_SW_OFF   - Switch is in the OFF position
     //
     // begin will return 0 on success
-    if (acc.begin(OSEPP_ACC_SW_ON) != 0)
-    {
-        Serial.println("Error connecting to accelerometer");
-        fail = true;
-        return;
-    }
-
+    acc.begin(OSEPP_ACC_SW_ON);
     acc.setSensitivity(ADXL345_RANGE_PM4G);
 }
 
 void loop()
 {
-    // don't bother reading if we failed to connect
-    if (fail)
-        return;
-
-    // 3 doubles to store the data
+    
     double x, y, z;
 
-    // Read Gs
-    // Note: You can also read the raw data by calling
-    //       readRaw() - readRaw takes int16_t instead of doubles
-    if (acc.readGs(&x, &y, &z) != 0)
-    {
-        Serial.println("Failed to read accelerometer");
-        return;
-    }
+    acc.readGs(&x, &y, &z);
     
     // print them out
     Serial.print("X: ");
